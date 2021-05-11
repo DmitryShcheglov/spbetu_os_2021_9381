4203a6b73cd0a8c133df7b6bedde3bfe1a6dc0ab
PtichkinSergey <sergey.ptichkin@gmail.com>
2021-05-12 00:05:23 +0300

    update

--- Ptichkin/lab6/src/lab2.asm
+++ Ptichkin/lab6/src/lab2.asm
@@ -1,165 +1,158 @@
-TESTPC  SEGMENT
+TESTPC SEGMENT
         ASSUME CS:TESTPC, DS:TESTPC, ES:NOTHING, SS:NOTHING
         ORG 100H
-START:  JMP BEGIN
-; Данные
-Unavailable_Mem_Address    db 'Segment address of the unavailable memory:    ',0DH,0AH,'$'
-Environment_Seg_Address    db 'Segment address of the environment:    ',0DH,0AH,'$'
-Command_Line_Tail          db 'The tail of the command line: ','$'
-Tail_Empty                 db 'Empty',0DH,0AH,'$'
-Environment_Area_Content   db 'Environment area content: ',0DH,0AH,'$'
-Module_Loaded_Path         db 'Path to loaded module: ','$'
-Tail                       db 128 DUP('$')
-Content                    db 128 DUP('$')
-Path                       db 128 DUP('$')
+START:
+        JMP BEGIN
 
-; Процедуры
-;-----------------------------------------------------
-TETR_TO_HEX PROC near 
-            and AL,0Fh
-            cmp AL,09
-            jbe NEXT
-            add AL,07
-NEXT:   add AL,30h
-        ret
-TETR_TO_HEX ENDP
-;-------------------------------
+UNAVAILABLE_ADDRESS db  'Address of unavailable memory:    ',0DH, 0AH, '$'
+ENVIROMENT_ADDRESS  db  'Address of the environment:      ',0DH, 0AH, '$'
+TAIL_MESSAGE        db  'Command line tail: ','$'
+NO_TAIL             db  'No arguments',0DH, 0AH, '$'
+ENVIRONMENT_MESSAGE db  'Content of environment: ',0DH, 0AH,'$'
+PATH_MESSAGE        db  'Path of the module: ','$'
+PATH_MODULE         db   83 DUP(0DH,'$')
+TAIL                db   83 DUP(0DH, 0AH,'$')
+ENVIRONMENT_CONTENT db   128 DUP('$')
+
+TETR_TO_HEX PROC near
+      and AL,0Fh
+      cmp AL,09
+      jbe NEXT
+      add AL,07
+NEXT: add AL,30h
+      ret
+      TETR_TO_HEX ENDP
+
 BYTE_TO_HEX PROC near
-; Байт в AL переводится в два символа шестн. числа AX
+      ; байт AL переводится в два символа 16с.с. числа в AX
             push CX
             mov AH,AL
             call TETR_TO_HEX
             xchg AL,AH
             mov CL,4
             shr AL,CL
-            call TETR_TO_HEX ; В AL Старшая цифра 
-            pop CX           ; В AH младшая цифра
+            call TETR_TO_HEX
+            pop CX
             ret
-BYTE_TO_HEX ENDP
-;-------------------------------
-WRD_TO_HEX PROC near
-; Перевод в 16 с/с 16-ти разрядного числа
-; в AX - число, DI - адрес последнего символа   
-        push BX
-        mov BH,AH
-        call BYTE_TO_HEX
-        mov [DI],AH
-        dec DI
-        mov [DI],AL
-        dec DI
-        mov AL,BH
-        call BYTE_TO_HEX
-        mov [DI],AH
-        dec DI
-        mov [DI],AL
-        pop BX
-        ret
-WRD_TO_HEX ENDP
-;--------------------------------------------------
+        BYTE_TO_HEX ENDP
 
-PRINT  PROC NEAR    ; вывод строки на экран
-      push ax
-      mov ah, 9h
-      int 21H
-      pop ax
-      ret
-PRINT ENDP
-;-------------------------------
-; КОД
+        WRD_TO_HEX PROC near
+        ;   в AX - число, DI - адрес последнего символа
+            push BX
+            mov BH,AH
+            call BYTE_TO_HEX
+            mov [DI],AH
+            dec DI
+            mov [DI],AL
+            dec DI
+            mov AL,BH
+            call BYTE_TO_HEX
+            mov [DI],AH
+            dec DI
+            mov [DI],AL
+            pop BX
+            ret
+        WRD_TO_HEX ENDP
+
+        PRINT  PROC NEAR    ; вывод строки на экран
+              push ax
+              mov ah, 9h
+              int 21h
+              pop ax
+              ret
+        PRINT ENDP
+
 BEGIN:
-;Сегментный адрес недоступной памяти
-      mov ax, ds:[0002h]
-	  mov dx, offset Unavailable_Mem_Address
-	  mov di, dx
-	  add di, 45
-	  call WRD_TO_HEX
-	  call PRINT
-;Сегментный адрес среды	  
-	  mov ax, ds:[002Ch]
-	  mov dx, offset Environment_Seg_Address
-	  mov di, dx
-	  add di, 38
-	  call WRD_TO_HEX
-	  call PRINT
-;Хвост командной строки	  
-	  mov cl, ds:[0080h]
-	  mov dx, offset Command_Line_Tail
-	  call PRINT
-	  cmp cl, 0
-	  je Empty_Tail 
-	  mov si, 0
-Tail_Loop:
-      mov al, ds:[81h + si]
-      mov Tail[si], al
-      inc si
-      loop Tail_Loop
-Print_Tail:
-	  mov al, 0Ah
-      mov Tail[si], al
-	  mov dx, offset Tail
-	  call PRINT
-	  jmp  Environment
-Empty_Tail:
-	  mov dx, offset Tail_Empty ;сообщение об отсутствии аргументов
-	  call PRINT
-;Содержимое области среды	  
-Environment:	
-	  mov dx, offset Environment_Area_Content
-	  call PRINT
-	  mov ax, ds:[2Ch]
-      mov ds, ax
-      mov di, offset Content
-      mov si, 0
-Read_content:
-	  lodsb
-	  cmp al, 0
-	  je EOL_OR_EXIT
-	  stosb
-	  jmp Read_content
-EOL_OR_EXIT:
-	  mov al, 0Ah
-      stosb
-	  lodsb
-	  cmp al, 0
-	  je End_Environment
-	  stosb
-	  jmp Read_content
-End_Environment:
-	  mov al, 0Dh
-      stosb
-      mov al, 36
-      stosb
-	  push ds
-      mov ax, es
-      mov ds, ax
-	  mov dx, offset Content
-	  call PRINT
-;Путь загружаемого модуля	  
-	  mov dx, offset Module_Loaded_Path
-      call PRINT
-      add si, 2
-      mov  ds, ds:[2Ch]
-      mov di, offset Path
-Read_Path:
-	  lodsb
-	  cmp al, 0
-	  je End_Path
-	  stosb
-	  jmp Read_Path
-End_Path:
-	  mov al, 0Ah
-      stosb
-      mov al, 0Dh
-      stosb
-      mov al, 36
-      stosb
-      mov ax, es
-      mov ds, ax
-	  mov dx, offset Path
-	  call PRINT
-; Выход в DOS
-        xor AL,AL
-        mov AH,4Ch
-        int 21H
-TESTPC  ENDS
-        END START ; Конец модуля, START - точка входа
+  mov ax, ds:[2h]
+  lea di, UNAVAILABLE_ADDRESS
+  add di, 34
+	call WRD_TO_HEX
+	lea dx, UNAVAILABLE_ADDRESS
+	call PRINT
+
+  mov ax, ds:[2Ch]
+  lea di, ENVIROMENT_ADDRESS
+  add di, 32
+	call WRD_TO_HEX
+	lea dx,  ENVIROMENT_ADDRESS
+	call PRINT
+
+  lea dx, TAIL_MESSAGE
+  call PRINT
+  mov cl, ds:[80h]
+  cmp cl, 0
+  je EMPTY
+  mov si, 0
+  lea bx, TAIL
+
+WRITE_TAIL:
+     mov dl, ds:[81h+si]
+     mov [bx+si], dl
+     inc si
+     loop WRITE_TAIL
+     lea dx, TAIL
+     call PRINT
+     jmp ENVIRONMENT
+
+EMPTY:
+    lea dx, NO_TAIL
+    call PRINT
+
+ENVIRONMENT:
+    lea dx, ENVIRONMENT_MESSAGE
+    call PRINT
+    lea di, ENVIRONMENT_CONTENT
+    mov		ax, ds:[2Ch]
+    mov		ds, ax
+    mov si, 0
+
+READ:
+    lodsb
+    cmp 	al, 0
+    jne 	END_
+
+END_LINE:
+    mov al, 0Ah
+    stosb
+    lodsb
+    cmp 	al, 0h
+    jne END_
+    mov byte ptr [di], 0Dh
+    mov byte ptr [di+1], '$'
+    mov bx, ds
+    mov ax, es
+    mov ds, ax
+    lea dx, ENVIRONMENT_CONTENT
+    call PRINT
+    jmp READING_PATH
+
+END_:
+    stosb
+    jmp READ
+
+READING_PATH:
+     lea dx, PATH_MESSAGE
+     call PRINT
+     add si, 2
+     mov  ds, ds:[2Ch]
+     lea di, PATH_MODULE
+CYCLE_PATH:
+     lodsb
+     cmp  al, 0
+     je  END_CYCLE
+     stosb
+     jmp     CYCLE_PATH
+ END_CYCLE:
+     mov  bx, ds
+     mov  ax, es
+     mov  ds, ax
+     lea dx, PATH_MODULE
+     call PRINT
+
+  xor al, al
+	mov ah,01h
+	int 21h
+	mov ah, 4Ch
+	int 21h
+  TESTPC ENDS
+      END START 
--- Ptichkin/lab6/src/lab6.asm
+++ Ptichkin/lab6/src/lab6.asm
@@ -1,252 +1,250 @@
-MY_STACK segment stack
-	dw 128 dup(?)
-MY_STACK ends
+STACK SEGMENT STACK
+	DW 128 DUP(?)
+STACK ENDS
 
-DATA segment
-	program db 'lab2.com', 0
-	block_parameters dw 0
-					dd 0
-					dd 0
-					dd 0
-	mem_flag db 0
+DATA SEGMENT
+	program db 'lab2.com', 0	
+	flag db 0
 	cmd db 1h, 0dh
-	pos db 128 dup(0)
-	keep_ss dw 0
-	keep_sp dw 0
-	keep_psp dw 0
+	pos db 128 DUP(0)
+	ssKeep DW 0
+	spKeep DW 0
+	pspKeep DW 0
+	blockParam DW 0
+               dd 0
+               dd 0
+               dd 0
 
-	McbCrashError db 'Error! MCB crashed!', 0dh, 0ah, '$'
-	NoMemoryError db 'Error! Not enough memory!', 0dh, 0ah, '$'
-	AddressError db 'Error! Invalid memory address!', 0dh, 0ah, '$'
-	FunctionNumberError db 'Error! Invalid function number', 0dh, 0ah, '$'
-	NoFileError db 'Error! Can not find the file!', 0dh, 0ah, '$'
-	DiskError db 'Error! Disk error!', 0dh, 0ah, '$'
-	MemoryError db 'Error! Insufficient memory', 0dh, 0ah, '$'
-	EnvironmentError db 'Error! Wrong string of environment ', 0dh, 0ah, '$'
-	WrongFormatError db 'Error! Wrong format', 0dh, 0ah, '$'
-	DeviceError db 0dh, 0ah, 'Program ended by device error' , 0dh, 0ah, '$'
+	StringMemoryFree db 'Memory was freed' , 0dh, 0ah, '$'
+    StringErrorCrash db 'Error! MCB crashed!', 0dh, 0ah, '$' 
+	StringErrorNoMemory db 'Error! Not enough memory!', 0dh, 0ah, '$' 
+	StringErrorAddress db 'Error! Invalid memory addressess!', 0dh, 0ah, '$'
+	StringErrorFunNumber db 'Error! Invalid function number!', 0dh, 0ah, '$' 
+	StringErrorNoFile db 'Error! File not found!', 0dh, 0ah, '$' 
+	StringErrorDisk db 'Error with disk!', 0dh, 0ah, '$' 
+	StringErrorMemory db 'Error! Insufficient memory!', 0dh, 0ah, '$' 
+	StringErrorEnviroment db 'Error! Wrong string of environment!', 0dh, 0ah, '$' 
+	StringErrorFormat db 'Error! Wrong format!', 0dh, 0ah, '$' 
+	StringErrorDevice db 0dh, 0ah, 'Error! Device error!' , 0dh, 0ah, '$'
+	StringEndCode db 0dh, 0ah, 'The program successfully ended with code:    ' , 0dh, 0ah, '$'
+	StringEndCtrl db 0dh, 0ah, 'The program was interrupted by ctrl-break' , 0dh, 0ah, '$'
+	StringEndInt db 0dh, 0ah, 'The program was ended by interruption int 31h' , 0dh, 0ah, '$'
 
-	FreeMemoryMessage db 'Memory has been freed' , 0dh, 0ah, '$'
+	dataEnd db 0
+DATA ENDS
 
-	EndedCode db 0dh, 0ah, 'Program ended with code    ' , 0dh, 0ah, '$'
-	EndedCtrl db 0dh, 0ah, 'Program ended by CTRL-break' , 0dh, 0ah, '$'
-	Ended31 db 0dh, 0ah, 'Program ended by int 31h' , 0dh, 0ah, '$'
-	end_data db 0
-DATA ends
+CODE SEGMENT
 
-CODE segment
+assume cs:CODE, ds:DATA, ss:STACK
 
-assume cs:CODE, ds:DATA, ss:MY_STACK
-
-PRINT proc
+output PROC 
  	push ax
  	mov ah, 09h
- 	int 21h
+ 	int 21h 
  	pop ax
  	ret
-PRINT endp
+output ENDP 
 
-MEMORY_FREE proc
+memoryFree PROC 
 	push ax
 	push bx
 	push cx
 	push dx
-
-	mov ax, offset end_data
-	mov bx, offset FINISH
+	
+	mov ax, offset dataEnd
+	mov bx, offset ENDProgram
 	add bx, ax
-
+	
 	mov cl, 4
 	shr bx, cl
 	add bx, 2bh
 	mov ah, 4ah
-	int 21h
+	int 21h 
 
-	jnc FINISH_FREE
-	mov mem_flag, 1
-
-CRASH_MCB:
+	jnc endMemoryFree
+	mov flag, 1
+	
+crashMCB:
 	cmp ax, 7
-	jne NOT_ENOUGH_MEMORY
-	mov dx, offset McbCrashError
-	call PRINT
-	jmp RET_F
-NOT_ENOUGH_MEMORY:
+	jne noMemory
+	mov dx, offset StringErrorCrash
+	call output
+	jmp retFunction	
+noMemory:
 	cmp ax, 8
-	jne ADDRESS_FAIL
-	mov dx, offset NoMemoryError
-	call PRINT
-	jmp RET_F
-ADDRESS_FAIL:
+	jne errorAddress
+	mov dx, offset StringErrorNoMemory
+	call output
+	jmp retFunction	
+errorAddress:
 	cmp ax, 9
-	mov dx, offset AddressError
-	call PRINT
-	jmp RET_F
-FINISH_FREE:
-	mov mem_flag, 1
-	mov dx, offset FreeMemoryMessage
-	call PRINT
-
-RET_F:
+	mov dx, offset StringErrorAddress
+	call output
+	jmp retFunction
+endMemoryFree:
+	mov flag, 1
+	mov dx, offset StringMemoryFree
+	call output
+	
+retFunction:
 	pop dx
 	pop cx
 	pop bx
 	pop ax
 	ret
-MEMORY_FREE endp
+memoryFree ENDP
 
-LOAD proc
+load PROC 
 	push ax
 	push bx
 	push cx
 	push dx
 	push ds
 	push es
-	mov keep_sp, sp
-	mov keep_ss, ss
-
+	
+	mov spKeep, sp
+	mov ssKeep, ss
+	
 	mov ax, DATA
 	mov es, ax
-	mov bx, offset block_parameters
+	mov bx, offset blockParam
 	mov dx, offset cmd
-	mov [bx+2], dx
-	mov [bx+4], ds
+	mov [bx + 2], dx
+	mov [bx + 4], ds 
 	mov dx, offset pos
-
-	mov ax, 4b00h
-	int 21h
-
-	mov ss, keep_ss
-	mov sp, keep_sp
+	
+	mov ax, 4b00h 
+	int 21h 
+	
+	mov ss, ssKeep
+	mov sp, spKeep
 	pop es
 	pop ds
-
+	
 	jnc loads
-
-F_N_ERROR:
+	
 	cmp ax, 1
-	jne FILE_ERROR
-	mov dx, offset FunctionNumberError
-	call PRINT
-	jmp load_end
-FILE_ERROR:
+	jne errorFile
+	mov dx, offset StringErrorFunNumber
+	call output
+	jmp loadExit
+errorFile:
 	cmp ax, 2
-	jne DISK_ERROR
-	mov dx, offset NoFileError
-	call PRINT
-	jmp load_end
-DISK_ERROR:
+	jne errorDisk
+	mov dx, offset StringErrorNoFile
+	call output
+	jmp loadExit
+errorDisk:
 	cmp ax, 5
-	jne MEMORY_ERROR
-	mov dx, offset DiskError
-	call PRINT
-	jmp load_end
-MEMORY_ERROR:
+	jne errorMemory
+	mov dx, offset StringErrorDisk
+	call output
+	jmp loadExit
+errorMemory:
 	cmp ax, 8
-	jne ENV_ERROR
-	mov dx, offset MemoryError
-	call PRINT
-	jmp load_end
-ENV_ERROR:
+	jne errorEnviroment
+	mov dx, offset StringErrorMemory
+	call output
+	jmp loadExit
+errorEnviroment:
 	cmp ax, 10
-	jne FORMAT_ERROR
-	mov dx, offset EnvironmentError
-	call PRINT
-	jmp load_end
-FORMAT_ERROR:
+	jne errorFormat
+	mov dx, offset StringErrorEnviroment
+	call output
+	jmp loadExit
+errorFormat:
 	cmp ax, 11
-	mov dx, offset WrongFormatError
-	call PRINT
-	jmp load_end
+	mov dx, offset StringErrorFormat
+	call output
+	jmp loadExit
 
 loads:
 	mov ah, 4dh
 	mov al, 00h
-	int 21h
-
+	int 21h 
+	
 	cmp ah, 0
-	jne CTRL_FUNC
-	push di
-	mov di, offset EndedCode
-	mov [di+26], al
+	jne jumpCtrl
+	push di 
+	mov di, offset StringEndCode
+	mov [di + 44], al 
 	pop si
-	mov dx, offset EndedCode
-	call PRINT
-	jmp load_end
-CTRL_FUNC:
+	mov dx, offset StringEndCode
+	call output 
+	jmp loadExit
+jumpCtrl:
 	cmp ah, 1
-	jne DEVICE
-	mov dx, offset EndedCtrl
-	call PRINT
-	jmp load_end
-DEVICE:
-	cmp ah, 2
-	jne INT_31
-	mov dx, offset DeviceError
-	call PRINT
-	jmp load_end
-INT_31:
+	jne device
+	mov dx, offset StringEndCtrl 
+	call output 
+	jmp loadExit
+device:
+	cmp ah, 2 
+	jne interruptionJump
+	mov dx, offset StringErrorDevice
+	call output 
+	jmp loadExit
+interruptionJump:
 	cmp ah, 3
-	mov dx, offset Ended31
-	call PRINT
+	mov dx, offset StringEndInt
+	call output 
 
-load_end:
+loadExit:
 	pop dx
 	pop cx
 	pop bx
 	pop ax
 	ret
-load endp
+load ENDP
 
-FIND_PATH proc
+path PROC 
 	push ax
 	push bx
-	push cx
+	push cx 
 	push dx
 	push di
 	push si
 	push es
-
-	mov ax, keep_psp
+	
+	mov ax, pspKeep
 	mov es, ax
 	mov es, es:[2ch]
 	mov bx, 0
-
-LOOKING_PATH:
+	
+findPath:
 	inc bx
-	cmp byte ptr es:[bx-1], 0
-	jne LOOKING_PATH
+	cmp byte ptr es:[bx - 1], 0
+	jne findPath
 
-	cmp byte ptr es:[bx+1], 0
-	jne LOOKING_PATH
-
+	cmp byte ptr es:[bx + 1], 0 
+	jne findPath
+	
 	add bx, 2
 	mov di, 0
-
-FIND_LOOP:
+	
+findLoop:
 	mov dl, es:[bx]
-	mov byte ptr [pos+di], dl
+	mov byte ptr [pos + di], dl
 	inc di
 	inc bx
 	cmp dl, 0
-	je QUIT_LOOP
+	je exitfindLoop
 	cmp dl, '\'
-	jne FIND_LOOP
+	jne findLoop
 	mov cx, di
-	jmp FIND_LOOP
-
-QUIT_LOOP:
+	jmp findLoop
+exitfindLoop:
 	mov di, cx
 	mov si, 0
-
-END_FN:
-	mov dl, byte ptr [program+si]
-	mov byte ptr [pos+di], dl
-	inc di
+	
+endFn:
+	mov dl, byte ptr [program + si]
+	mov byte ptr [pos + di], dl
+	inc di 
 	inc si
-	cmp dl, 0
-	jne END_FN
-
+	cmp dl, 0 
+	jne endFn
+		
 	pop es
 	pop si
 	pop di
@@ -255,27 +253,29 @@ END_FN:
 	pop bx
 	pop ax
 	ret
-FIND_PATH endp
+path ENDP
 
-BEGIN proc far
+begin PROC far
 	push ds
 	xor ax, ax
 	push ax
+	
 	mov ax, DATA
 	mov ds, ax
-	mov keep_psp, es
-	call MEMORY_FREE
-	cmp mem_flag, 0
-	je QUIT
-	call FIND_PATH
-	call LOAD
-QUIT:
+	mov pspKeep, es
+	
+	call memoryFree 
+	cmp flag, 0
+	je exit
+	call path
+	call load
+	
+exit:
 	xor al, al
 	mov ah, 4ch
 	int 21h
+begin ENDP
 
-BEGIN   endp
-
-FINISH:
-CODE ends
-end BEGIN 
+ENDProgram:
+CODE ENDS
+END begin
