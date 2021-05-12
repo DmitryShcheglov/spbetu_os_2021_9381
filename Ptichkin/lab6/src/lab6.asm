cdd2015f2204a3fc5f129cadd96332c26ed4a76a
PtichkinSergey <sergey.ptichkin@gmail.com>
2021-05-12 18:28:01 +0300

    fix

--- Ptichkin/lab6/src/lab6.asm
+++ Ptichkin/lab6/src/lab6.asm
@@ -7,52 +7,52 @@ DATA SEGMENT
 	flag db 0
 	cmd db 1h, 0dh
 	pos db 128 DUP(0)
-	ssKeep DW 0
-	spKeep DW 0
-	pspKeep DW 0
-	blockParam DW 0
+	keep_ss DW 0
+	keep_sp DW 0
+	keep_psp DW 0
+	Block_param DW 0
                dd 0
                dd 0
                dd 0
 
-	StringMemoryFree db 'Memory was freed' , 0dh, 0ah, '$'
-    StringErrorCrash db 'Error! MCB crashed!', 0dh, 0ah, '$' 
-	StringErrorNoMemory db 'Error! Not enough memory!', 0dh, 0ah, '$' 
-	StringErrorAddress db 'Error! Invalid memory addressess!', 0dh, 0ah, '$'
-	StringErrorFunNumber db 'Error! Invalid function number!', 0dh, 0ah, '$' 
-	StringErrorNoFile db 'Error! File not found!', 0dh, 0ah, '$' 
-	StringErrorDisk db 'Error with disk!', 0dh, 0ah, '$' 
-	StringErrorMemory db 'Error! Insufficient memory!', 0dh, 0ah, '$' 
-	StringErrorEnviroment db 'Error! Wrong string of environment!', 0dh, 0ah, '$' 
-	StringErrorFormat db 'Error! Wrong format!', 0dh, 0ah, '$' 
-	StringErrorDevice db 0dh, 0ah, 'Error! Device error!' , 0dh, 0ah, '$'
-	StringEndCode db 0dh, 0ah, 'The program successfully ended with code:    ' , 0dh, 0ah, '$'
-	StringEndCtrl db 0dh, 0ah, 'The program was interrupted by ctrl-break' , 0dh, 0ah, '$'
-	StringEndInt db 0dh, 0ah, 'The program was ended by interruption int 31h' , 0dh, 0ah, '$'
+	Memory_free_message db 'Memory has been freed' , 0dh, 0ah, '$'
+    Error_crash_message db 'Error! MCB crashed!', 0dh, 0ah, '$' 
+	Error_no_memory_message db 'Error! Not enough memory!', 0dh, 0ah, '$' 
+	Error_address_message db 'Error! Invalid memory address!', 0dh, 0ah, '$'
+	Error_fun_num_message db 'Error! Invalid function number!', 0dh, 0ah, '$' 
+	Error_no_file_message db 'Error! File not found!', 0dh, 0ah, '$' 
+	Error_disk_message db 'Error with disk!', 0dh, 0ah, '$' 
+	Error_memory_message db 'Error! Insufficient memory!', 0dh, 0ah, '$' 
+	Error_enviroment_message db 'Error! Wrong string of environment!', 0dh, 0ah, '$' 
+	Error_format_message db 'Error! Wrong format!', 0dh, 0ah, '$' 
+	Error_Device_message db 0dh, 0ah, 'Error! Device error!' , 0dh, 0ah, '$'
+	End_code_message db 0dh, 0ah, 'The program successfully ended with code:    ' , 0dh, 0ah, '$'
+	End_ctrl_message db 0dh, 0ah, 'The program was interrupted by ctrl-break' , 0dh, 0ah, '$'
+	End_int_message db 0dh, 0ah, 'The program was ended by interruption int 31h' , 0dh, 0ah, '$'
 
-	dataEnd db 0
+	End_data db 0
 DATA ENDS
 
 CODE SEGMENT
 
 assume cs:CODE, ds:DATA, ss:STACK
 
-output PROC 
+PRINT PROC 
  	push ax
  	mov ah, 09h
  	int 21h 
  	pop ax
  	ret
-output ENDP 
+PRINT ENDP 
 
-memoryFree PROC 
+FREE_MEM PROC 
 	push ax
 	push bx
 	push cx
 	push dx
 	
-	mov ax, offset dataEnd
-	mov bx, offset ENDProgram
+	mov ax, offset End_data
+	mov bx, offset end_program
 	add bx, ax
 	
 	mov cl, 4
@@ -61,40 +61,40 @@ memoryFree PROC
 	mov ah, 4ah
 	int 21h 
 
-	jnc endMemoryFree
+	jnc End_mem_free
 	mov flag, 1
 	
-crashMCB:
+Crash_mcb:
 	cmp ax, 7
-	jne noMemory
-	mov dx, offset StringErrorCrash
-	call output
-	jmp retFunction	
-noMemory:
+	jne No_mem
+	mov dx, offset Error_crash_message
+	call PRINT
+	jmp Ret_func	
+No_mem:
 	cmp ax, 8
-	jne errorAddress
-	mov dx, offset StringErrorNoMemory
-	call output
-	jmp retFunction	
-errorAddress:
+	jne Error_address
+	mov dx, offset Error_no_memory_message
+	call PRINT
+	jmp Ret_func	
+Error_address:
 	cmp ax, 9
-	mov dx, offset StringErrorAddress
-	call output
-	jmp retFunction
-endMemoryFree:
+	mov dx, offset Error_address_message
+	call PRINT
+	jmp Ret_func
+End_mem_free:
 	mov flag, 1
-	mov dx, offset StringMemoryFree
-	call output
+	mov dx, offset Memory_free_message
+	call PRINT
 	
-retFunction:
+Ret_func:
 	pop dx
 	pop cx
 	pop bx
 	pop ax
 	ret
-memoryFree ENDP
+FREE_MEM ENDP
 
-load PROC 
+LOAD PROC 
 	push ax
 	push bx
 	push cx
@@ -102,12 +102,12 @@ load PROC
 	push ds
 	push es
 	
-	mov spKeep, sp
-	mov ssKeep, ss
+	mov keep_sp, sp
+	mov keep_ss, ss
 	
 	mov ax, DATA
 	mov es, ax
-	mov bx, offset blockParam
+	mov bx, offset Block_param
 	mov dx, offset cmd
 	mov [bx + 2], dx
 	mov [bx + 4], ds 
@@ -116,88 +116,88 @@ load PROC
 	mov ax, 4b00h 
 	int 21h 
 	
-	mov ss, ssKeep
-	mov sp, spKeep
+	mov ss, keep_ss
+	mov sp, keep_sp
 	pop es
 	pop ds
 	
-	jnc loads
+	jnc Loads
 	
 	cmp ax, 1
-	jne errorFile
-	mov dx, offset StringErrorFunNumber
-	call output
-	jmp loadExit
-errorFile:
+	jne Error_file
+	mov dx, offset Error_fun_num_message
+	call PRINT
+	jmp Load_exit
+Error_file:
 	cmp ax, 2
-	jne errorDisk
-	mov dx, offset StringErrorNoFile
-	call output
-	jmp loadExit
-errorDisk:
+	jne Error_Disk
+	mov dx, offset Error_no_file_message
+	call PRINT
+	jmp Load_exit
+Error_Disk:
 	cmp ax, 5
-	jne errorMemory
-	mov dx, offset StringErrorDisk
-	call output
-	jmp loadExit
-errorMemory:
+	jne Error_mem
+	mov dx, offset Error_disk_message
+	call PRINT
+	jmp Load_exit
+Error_mem:
 	cmp ax, 8
-	jne errorEnviroment
-	mov dx, offset StringErrorMemory
-	call output
-	jmp loadExit
-errorEnviroment:
+	jne Error_enviroment
+	mov dx, offset Error_memory_message
+	call PRINT
+	jmp Load_exit
+Error_enviroment:
 	cmp ax, 10
-	jne errorFormat
-	mov dx, offset StringErrorEnviroment
-	call output
-	jmp loadExit
-errorFormat:
+	jne Error_format
+	mov dx, offset Error_enviroment_message
+	call PRINT
+	jmp Load_exit
+Error_format:
 	cmp ax, 11
-	mov dx, offset StringErrorFormat
-	call output
-	jmp loadExit
+	mov dx, offset Error_format_message
+	call PRINT
+	jmp Load_exit
 
-loads:
+Loads:
 	mov ah, 4dh
 	mov al, 00h
 	int 21h 
 	
 	cmp ah, 0
-	jne jumpCtrl
+	jne Jump_ctrl
 	push di 
-	mov di, offset StringEndCode
+	mov di, offset End_code_message
 	mov [di + 44], al 
 	pop si
-	mov dx, offset StringEndCode
-	call output 
-	jmp loadExit
-jumpCtrl:
+	mov dx, offset End_code_message
+	call PRINT 
+	jmp Load_exit
+Jump_ctrl:
 	cmp ah, 1
-	jne device
-	mov dx, offset StringEndCtrl 
-	call output 
-	jmp loadExit
-device:
+	jne Device
+	mov dx, offset End_ctrl_message 
+	call PRINT 
+	jmp Load_exit
+Device:
 	cmp ah, 2 
-	jne interruptionJump
-	mov dx, offset StringErrorDevice
-	call output 
-	jmp loadExit
-interruptionJump:
+	jne Interrup_jump
+	mov dx, offset Error_Device_message
+	call PRINT 
+	jmp Load_exit
+Interrup_jump:
 	cmp ah, 3
-	mov dx, offset StringEndInt
-	call output 
+	mov dx, offset End_int_message
+	call PRINT 
 
-loadExit:
+Load_exit:
 	pop dx
 	pop cx
 	pop bx
 	pop ax
 	ret
-load ENDP
+LOAD ENDP
 
-path PROC 
+PATH PROC 
 	push ax
 	push bx
 	push cx 
@@ -206,44 +206,44 @@ path PROC
 	push si
 	push es
 	
-	mov ax, pspKeep
+	mov ax, keep_psp
 	mov es, ax
 	mov es, es:[2ch]
 	mov bx, 0
 	
-findPath:
+Find_path:
 	inc bx
 	cmp byte ptr es:[bx - 1], 0
-	jne findPath
+	jne Find_path
 
 	cmp byte ptr es:[bx + 1], 0 
-	jne findPath
+	jne Find_path
 	
 	add bx, 2
 	mov di, 0
 	
-findLoop:
+Find_loop:
 	mov dl, es:[bx]
 	mov byte ptr [pos + di], dl
 	inc di
 	inc bx
 	cmp dl, 0
-	je exitfindLoop
+	je Exit_find_loop
 	cmp dl, '\'
-	jne findLoop
+	jne Find_loop
 	mov cx, di
-	jmp findLoop
-exitfindLoop:
+	jmp Find_loop
+Exit_find_loop:
 	mov di, cx
 	mov si, 0
 	
-endFn:
+End_fn:
 	mov dl, byte ptr [program + si]
 	mov byte ptr [pos + di], dl
 	inc di 
 	inc si
 	cmp dl, 0 
-	jne endFn
+	jne End_fn
 		
 	pop es
 	pop si
@@ -253,29 +253,26 @@ endFn:
 	pop bx
 	pop ax
 	ret
-path ENDP
+PATH ENDP
 
-begin PROC far
+MAIN PROC far
 	push ds
 	xor ax, ax
 	push ax
-	
 	mov ax, DATA
 	mov ds, ax
-	mov pspKeep, es
-	
-	call memoryFree 
+	mov keep_psp, es
+	call FREE_MEM 
 	cmp flag, 0
 	je exit
-	call path
-	call load
-	
+	call PATH
+	call LOAD
 exit:
 	xor al, al
 	mov ah, 4ch
 	int 21h
-begin ENDP
+MAIN ENDP
 
-ENDProgram:
+end_program:
 CODE ENDS
-END begin
+END MAIN
