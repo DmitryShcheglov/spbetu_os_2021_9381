a49f1992b4a66b584971858b32dd169a5bdfe457
EFFECT322 <55045221+EFFECT322@users.noreply.github.com>
2021-05-26 18:56:21 +0300

    Upload lab2 2

--- Nikolaev/lab6/src/lab2.asm
+++ Nikolaev/lab6/src/lab2.asm
@@ -1,147 +1,157 @@
-CODE		SEGMENT	
-ASSUME 	CS:CODE, DS:CODE, ES:NOTHING, SS:NOTHING
-ORG		100H
-START:	JMP	BEGIN
-STR1		DB "Memory Address:      ", 13,10, "$"
-STR2		DB "Env. Address:      ", 13,10, "$"
-STR3		DB "Tail:                                         ", 13,10, "$"
-STR4		DB "Env. Data: ", 13, 10, "$"
-STR5		DB 13, 10, "$"
-STR6		DB "Path: ", 13, 10, "$"
+TESTPC SEGMENT
+        ASSUME CS:TESTPC, DS:TESTPC, ES:NOTHING, SS:NOTHING
+        ORG 100H
+START:
+        JMP BEGIN
 
-PRINT  	PROC	NEAR
-       	PUSH	AX
-       	MOV		AH, 09H
-        INT		21H
-		POP 	AX 
-        RET
-PRINT  	ENDP
+UNAVAILABLE_ADDRESS db  'Address of unavailable memory:    ',0DH, 0AH, '$'
+ENVIROMENT_ADDRESS  db  'Address of the environment:      ',0DH, 0AH, '$'
+TAIL_MESSAGE        db  'Command line tail: ','$'
+NO_TAIL             db  'No arguments',0DH, 0AH, '$'
+ENVIRONMENT_MESSAGE db  'Content of environment: ',0DH, 0AH,'$'
+PATH_MESSAGE        db  'Path of the module: ','$'
+PATH_MODULE         db   83 DUP(0DH,'$')
+TAIL                db   83 DUP(0DH, 0AH,'$')
+ENVIRONMENT_CONTENT db   128 DUP('$')
 
-TETR_TO_HEX	PROC	NEAR
+TETR_TO_HEX PROC near
+      and AL,0Fh
+      cmp AL,09
+      jbe NEXT
+      add AL,07
+NEXT: add AL,30h
+      ret
+      TETR_TO_HEX ENDP
 
-        AND		AL, 0FH
-        CMP		AL, 09H
-        JBE		NEXT
-      	ADD		AL, 07H
-    NEXT:      
-       	ADD		AL, 30H
-       	RET
-TETR_TO_HEX	ENDP
+BYTE_TO_HEX PROC near
+      ; байт AL переводится в два символа 16с.с. числа в AX
+            push CX
+            mov AH,AL
+            call TETR_TO_HEX
+            xchg AL,AH
+            mov CL,4
+            shr AL,CL
+            call TETR_TO_HEX
+            pop CX
+            ret
+        BYTE_TO_HEX ENDP
 
-BYTE_TO_HEX	PROC	NEAR    	
-       	PUSH	CX
-      	MOV	AH, AL
-       	CALL	TETR_TO_HEX
-        XCHG	AL, AH
-        MOV	CL, 4H
-        SHR	AL, CL
-       	CALL	TETR_TO_HEX
-        POP	CX
-        RET
-BYTE_TO_HEX	ENDP
+        WRD_TO_HEX PROC near
+        ;   в AX - число, DI - адрес последнего символа
+            push BX
+            mov BH,AH
+            call BYTE_TO_HEX
+            mov [DI],AH
+            dec DI
+            mov [DI],AL
+            dec DI
+            mov AL,BH
+            call BYTE_TO_HEX
+            mov [DI],AH
+            dec DI
+            mov [DI],AL
+            pop BX
+            ret
+        WRD_TO_HEX ENDP
 
+        PRINT  PROC NEAR    ; вывод строки на экран
+              push ax
+              mov ah, 9h
+              int 21h
+              pop ax
+              ret
+        PRINT ENDP
 
-WREAD_TO_HEX	PROC	NEAR
-        PUSH	BX
-        MOV 	BH, AH
-        CALL	BYTE_TO_HEX
-        MOV		[DI], AH
-        DEC		DI
-        MOV		[DI], AL
-        DEC		DI
-        MOV		AL, BH
-       	CALL	BYTE_TO_HEX
-       	MOV		[DI], AH
-       	DEC		DI
-       	MOV		[DI], AL
-       	POP		BX
-       	RET
-WREAD_TO_HEX	ENDP
+BEGIN:
+  mov ax, ds:[2h]
+  lea di, UNAVAILABLE_ADDRESS
+  add di, 34
+	call WRD_TO_HEX
+	lea dx, UNAVAILABLE_ADDRESS
+	call PRINT
 
+  mov ax, ds:[2Ch]
+  lea di, ENVIROMENT_ADDRESS
+  add di, 32
+	call WRD_TO_HEX
+	lea dx,  ENVIROMENT_ADDRESS
+	call PRINT
 
-BEGIN:          	
-		MOV		AX, DS:[02H]
-		MOV		DI, OFFSET STR1
-		ADD		DI, 19
-		CALL	WREAD_TO_HEX
-		MOV		DX, OFFSET STR1
-		CALL	PRINT
+  lea dx, TAIL_MESSAGE
+  call PRINT
+  mov cl, ds:[80h]
+  cmp cl, 0
+  je EMPTY
+  mov si, 0
+  lea bx, TAIL
 
-       	MOV		AX, DS:[2CH]
-		MOV		DI, OFFSET STR2
-		ADD		DI, 17
-		CALL	WREAD_TO_HEX
-		MOV		DX, OFFSET STR2
-		CALL	PRINT
+WRITE_TAIL:
+     mov dl, ds:[81h+si]
+     mov [bx+si], dl
+     inc si
+     loop WRITE_TAIL
+     lea dx, TAIL
+     call PRINT
+     jmp ENVIRONMENT
 
-		XOR		CX, CX
-       	XOR		SI, SI
-		MOV		CL, DS:[80H]
-		CMP		CL, 0
-		JE		NO_TAIL
-		MOV		DI, OFFSET STR3		
-		ADD		DI, 6H
-			
+EMPTY:
+    lea dx, NO_TAIL
+    call PRINT
+
+ENVIRONMENT:
+    lea dx, ENVIRONMENT_MESSAGE
+    call PRINT
+    lea di, ENVIRONMENT_CONTENT
+    mov		ax, ds:[2Ch]
+    mov		ds, ax
+    mov si, 0
+
 READ:
-		MOV		AL, DS:[81H + SI]
-		MOV		[DI], AL
-		INC		DI
-		INC		SI
-		LOOP	READ	
-NO_TAIL:
-		MOV	DX, OFFSET STR3
-		CALL	PRINT
-		MOV	DX, OFFSET STR4
-		CALL	PRINT
-		XOR	DI, DI
-		MOV 	BX, 2CH
-		MOV 	DS, [BX]
-BEGIN_STRING:
-		CMP 	BYTE PTR [DI], 00H
-		JE		ENTR
-		MOV 	DL, [DI]
-		MOV 	AH, 02H
-		INT 	21H
-		JMP 	END_DATA
-ENTR:
-		PUSH 	DS
-		MOV 	CX, CS
-		MOV 	DS, CX
-		MOV 	DX, OFFSET STR5
-		CALL 	PRINT
-		POP 	DS
-END_DATA:
-		INC 	DI
-		CMP 	WORD PTR [DI], 0001H
-		JE 		PATH
-		JMP 	BEGIN_STRING  
-PATH:
-		PUSH 	DS
-		MOV 	AX, CS
-       	MOV 	DS, AX
-		MOV	DX, OFFSET STR6
-		CALL 	PRINT
-		POP 	DS
-		ADD 	DI, 2	
-CIRCLE:
-		CMP 	BYTE PTR [DI], 00H
-		JE 		END_PATH
-		MOV 	DL, [DI]
-		MOV 	AH, 02H
-		INT 	21H
-		INC 	DI
-		JMP 	CIRCLE
-END_PATH:
-		PUSH 	DS
-		MOV 	CX, CS
-		MOV 	DS, CX
-		MOV 	DX, OFFSET STR5
-		CALL 	PRINT
-		POP 	DS
+    lodsb
+    cmp 	al, 0
+    jne 	END_
 
-		XOR		AL, AL
-		MOV		AH, 4CH
-		INT		21H
-      	 		
-CODE		ENDS
-END 		START
+END_LINE:
+    mov al, 0Ah
+    stosb
+    lodsb
+    cmp 	al, 0h
+    jne END_
+    mov byte ptr [di], 0Dh
+    mov byte ptr [di+1], '$'
+    mov bx, ds
+    mov ax, es
+    mov ds, ax
+    lea dx, ENVIRONMENT_CONTENT
+    call PRINT
+    jmp READING_PATH
+
+END_:
+    stosb
+    jmp READ
+
+READING_PATH:
+     lea dx, PATH_MESSAGE
+     call PRINT
+     add si, 2
+     mov  ds, ds:[2Ch]
+     lea di, PATH_MODULE
+CYCLE_PATH:
+     lodsb
+     cmp  al, 0
+     je  END_CYCLE
+     stosb
+     jmp     CYCLE_PATH
+ END_CYCLE:
+     mov  bx, ds
+     mov  ax, es
+     mov  ds, ax
+     lea dx, PATH_MODULE
+     call PRINT
+
+  xor al, al
+	mov ah,01h
+	int 21h
+	mov ah, 4Ch
+	int 21h
+  TESTPC ENDS
